9a1d02d3261d80c4e88e97fc9b58a1514d33eb58
Teddy van Jerry <me@teddy-van-jerry.org>
2022-06-10 22:06:53 +0800

    Deal with MSVC Secure Warning

--- src/astyle.h
+++ src/astyle.h
@@ -30,6 +30,11 @@
 #ifndef ASTYLE_H
 #define ASTYLE_H
 
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning (disable: 4267)
+#endif
+
 #include "compiler_defines.h"
 
 #include <string>
